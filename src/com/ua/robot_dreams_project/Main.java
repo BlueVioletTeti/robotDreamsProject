c94ffd4138fb4aaddbd15742f26c28426033ae60
Tetiana <tetiana.kolomiiets.fop@gmail.com>
2023-01-25 00:27:49 +0200

    modified .gitignore and formatting

--- src/com/ua/robot_dreams_project/Main.java
+++ src/com/ua/robot_dreams_project/Main.java
@@ -4,13 +4,13 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello World!!!");
-        int a=12;
-        int b=5;
-        System.out.println(a+b);
-        System.out.println(a-b);
-        System.out.println(a/b);
-        System.out.println((float)a/b);
-        System.out.println(a*b);
+        int a = 12;
+        int b = 5;
+        System.out.println(a + b);
+        System.out.println(a - b);
+        System.out.println(a / b);
+        System.out.println((float) a / b);
+        System.out.println(a * b);
 
     }
 
