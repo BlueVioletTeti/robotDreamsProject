a973df8bf9f81eabdacd94a6bf45521149a30b94
Tetiana <tetiana.kolomiiets.fop@gmail.com>
2023-02-02 18:53:32 +0200

    HomeWork #6 (Lecture 7) - refactored multiplySymbolNumberTimes methods and added fibonacci using recursion method

--- src/com/ua/robot_dreams_project/home_work7/Main.java
+++ src/com/ua/robot_dreams_project/home_work7/Main.java
@@ -3,10 +3,11 @@ package com.ua.robot_dreams_project.home_work7;
 public class Main {
     public static void main(String[] args) {
         System.out.println(numberToThePower(5, 3));
-        System.out.println(multiplySymbolNumberTimes(7));
-        System.out.println(multiplySymbolNumberTimes("%", 9));
-//      Extra experiment
+        multiplySymbolNumberTimes(7);
+        multiplySymbolNumberTimes("%", 9);
+//      Extra experiments
         System.out.println(multiplySymbolNumberTimes("Gleep", "! ", 17));
+        fibonacci(0, 1, 100);
     }
 
     public static int numberToThePower(int number, int power) {
@@ -14,16 +15,16 @@ public class Main {
         return number;
     }
 
-    public static String multiplySymbolNumberTimes(int number) {
-        return multiplySymbolNumberTimes("*", number);
+    public static void multiplySymbolNumberTimes(int number) {
+        multiplySymbolNumberTimes("*", number);
     }
 
-    public static String multiplySymbolNumberTimes(String symbol, int number) {
+    public static void multiplySymbolNumberTimes(String symbol, int number) {
         String symbolNumberTimes = "";
         for (int i = 0; i < number; i++) {
             symbolNumberTimes += symbol;
         }
-        return symbolNumberTimes;
+        System.out.println(symbolNumberTimes);
     }
 
     public static String multiplySymbolNumberTimes(String text, String separator, int number) {
@@ -33,4 +34,16 @@ public class Main {
         }
         return textNumberTimes.trim();
     }
+
+
+    public static void fibonacci(int previousNumber, int currentNumber, int limit) {
+        int result = previousNumber+currentNumber;
+        previousNumber = currentNumber;
+        currentNumber=result;
+        if (previousNumber < limit) {
+            System.out.print(previousNumber+" ");
+            fibonacci(previousNumber,currentNumber,limit);
+        }
+    }
 }
+
