6a222d9dd0fcb5e88cc3a22c060d4df89593e485
Tetiana <tetiana.kolomiiets.fop@gmail.com>
2023-02-22 13:09:35 +0200

    HomeWork #11 (Lecture 12)

---
+++ src/com/ua/robot_dreams_project/home_work12/Cat.java
@@ -0,0 +1,15 @@
+package com.ua.robot_dreams_project.home_work12;
+
+public class Cat extends Animal {
+    static {
+        System.out.println(2);
+    }
+
+    {
+        System.out.println(5);
+    }
+
+    public Cat() {
+        System.out.println(6);
+    }
+}
---
+++ src/com/ua/robot_dreams_project/home_work12/Main.java
@@ -0,0 +1,10 @@
+package com.ua.robot_dreams_project.home_work12;
+
+public class Main {
+    public static void main(String[] args) {
+//        Створити клас з усіма блоками для ініціалізації об’єкту в яких потрібно вивести
+//        на консоль числа - які відображають його порядковий номер ініціалізації
+
+        Cat cat = new Cat();
+    }
+}
